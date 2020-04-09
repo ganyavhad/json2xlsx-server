6c13d66818929796b8e7b3f9eabc7afea859211c
Ganesh Avhad <ganesh.avhad@wohlig.com>
2020-04-09 22:13:51 +0530

    file upload done

--- controller/JSONDataController.js
+++ controller/JSONDataController.js
@@ -2,7 +2,31 @@ const Router = require("express").Router();
 
 const JSONDataModel = require("../model/JSONDataModel");
 
+var multer = require("multer");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(" ").join("-");
+    cb(null, name);
+  }
+});
+
 Router.get("/", (req, res) => {
   res.json("Testing");
 });
+Router.post(
+  "/upload",
+  multer({storage: storage}).single("file"),
+  async (req, res) => {
+    try {
+      console.log("File", req.file);
+      await JSONDataModel.upload(req.file);
+      res.status(201).send("File uploaded successfully");
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+);
 module.exports = Router;
--- model/JSONDataModel.js
+++ model/JSONDataModel.js
@@ -1,4 +1,18 @@
 const JSONData = require("../service/JSONData");
+const fs = require("fs");
 
-let model = {};
+let model = {
+  upload: async (data) => {
+    try {
+      console.log(data.filename);
+      let uploadedData = fs.readFileSync(`uploads/${data.filename}`, "utf8");
+      console.log(JSON.parse(uploadedData).length);
+      const dataToSave = JSON.parse(uploadedData);
+      await JSONData.insertMany(dataToSave);
+      return;
+    } catch (err) {
+      throw err;
+    }
+  }
+};
 module.exports = model;
