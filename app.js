e102eaee8c1a0c30c569ac393bcf9cc4056cc06f
Ganesh Avhad <ganesh.avhad@wohlig.com>
2020-04-12 12:09:59 +0530

    cors policy setup

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 const mongoose = require("mongoose");
 const JSONDataController = require("./controller/JSONDataController");
 mongoose
